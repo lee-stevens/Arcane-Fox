233b4488f2c674abffbe6e2b85297754a80128a1
Lee Stevens <lee.stevens@atomus.com>
2024-10-10 14:22:25 +0100

    Created a common css file for global styles; Created a Dashboard component which can be toggled via a button; Simplified some of the JSX into shorthand notations; Added some notes re how React works;

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
+import './App.css'
+import './styles/common.css'
+import DashboardComponent from './components/DashboardComponent'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
+  const [showDashboard, setShowDashboard] = useState(false)
+
+  // React, just like Angular are both focused on Immutability for change detection
+  // When incrementing don't use ++ as that mutates the number, instead do x + 1 which returns a new value & reference
+
+  const incremementCount = () => setCount(count + 1);
+  const toggleDashboard = () => setShowDashboard(showDashboard => !showDashboard);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => toggleDashboard()}>
+          Show Dashboard: {showDashboard}
+        </button>
+        { /* Incremenet count using count + 1 instead of ++ because it creates a new value & reference for change detection to work */ }
+        <button onClick={() => incremementCount()}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      { /* Conditional JSX via Short Circuit Rendering - If JS statement is true then render encased JSX */ }
+      {showDashboard && (
+        <div>
+          <DashboardComponent logo={reactLogo} />
+        </div>
+      )}
     </>
   )
 }
-
-export default App
---
+++ src/components/DashboardComponent.tsx
@@ -0,0 +1,32 @@
+import '../App.css'
+import './DashboardComponent.css'
+
+//Conditional JSX via 'Function-based conditional rendering'
+function HeaderLogo(props: { logo?: string }): JSX.Element | undefined {
+  if (props.logo) {
+    return (
+      <a href="https://github.com/lee-stevens" target="_blank">
+        <img src={props.logo} className="logo" alt="Logo" />
+      </a>
+    )
+  }
+}
+
+//Destructuring the input params
+export default function DashboardComponent(props: { logo?: string }) {
+  const headerLogo = HeaderLogo(props)
+
+  return (
+    <>
+      {headerLogo} { /* Conditional JSX - Only renders if truthy */}
+      <h2>Dashboard</h2>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+
