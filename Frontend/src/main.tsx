1a28ad5fde44a52b04f0a2f7d041fd53fffbb1f1
Lee Stevens <Leepeterstevens@gmail.com>
2024-10-14 22:51:57 +0100

    Added tsconfig.json back in copied from earlier in the commit history; Path Aliases is working and the IDE is fine;

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import React from 'react'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
