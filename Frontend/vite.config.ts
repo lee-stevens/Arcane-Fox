4869648891f54976beee88e2c8b17200675283b1
Lee Stevens <Leepeterstevens@gmail.com>
2024-10-14 23:05:40 +0100

    The css postprocessor is now using the modern version instead of the default deprecated one;

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
+/* scss requires the modern-compiler api or it will default to the deprecated one
+https://stackoverflow.com/questions/78997907/the-legacy-js-api-is-deprecated-and-will-be-removed-in-dart-sass-2-0-0
+ */
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  },
   resolve: { // This must match tsconfig.app.json's alias'
     alias: {
       "@Routes": `${path.resolve(__dirname, "./src/components/Routes")}`,
